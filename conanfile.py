80f0c7dcadf875fc82ecf4ce2bf9a3773e5b4c95
Khalil Estell <kammce@google.com>
2021-09-24 18:37:08 -0700

    Fix requirments

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake
 from conan.tools.layout import cmake_layout
 
 
-class LibARMCortexConan(ConanFile):
+class liblpc40xx_conan(ConanFile):
     name = "liblpc40xx"
     version = "0.0.1"
     license = "Apache License Version 2.0"
@@ -11,35 +11,30 @@ class LibARMCortexConan(ConanFile):
     url = "https://github.com/SJSU-Dev2/liblpc40xx"
     description = "A collection of interfaces and abstractions for embedded peripherals and devices using modern C++"
     topics = ("peripherals", "hardware")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
     exports_sources = "CMakeLists.txt", "liblpc40xx/*"
-    generators = "cmake_find_package"
-
-    def config_options(self):
-        pass
-
-    def layout(self):
-        cmake_layout(self)
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package_multi"
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        cmake_debug = CMake(self)
+        cmake_debug.configure()
+        cmake_debug.build()
 
     def package(self):
         cmake = CMake(self)
+        self.copy("*.hpp")
+        self.copy("*.cpp")
         cmake.install()
 
     def package_info(self):
+        self.cpp_info.includedirs = ["."]
         self.cpp_info.libs = ["liblpc40xx"]
 
     def requirements(self):
-        self.requires("libembeddedhal/0.0.1@demo/testing")
-        self.requires("libxbitset/0.0.1@demo/testing")
-        self.requires("libarmcortex/0.0.1@demo/testing")
+        self.requires("libembeddedhal/0.0.1@")
+        self.requires("libxbitset/0.0.1@")
+        self.requires("libarmcortex/0.0.1@")
