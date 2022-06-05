06dedef3ecf5f818129d3cd3502c592ba628fbea
Khalil Estell <kammcecorp@gmail.com>
2022-06-05 11:38:21 -0700

    Name change: SJSU-Dev2 -> libembeddedhal

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class liblpc40xx_conan(ConanFile):
     version = "0.0.1"
     license = "Apache License Version 2.0"
     author = "Khalil Estell"
-    url = "https://github.com/SJSU-Dev2/liblpc40xx"
+    url = "https://github.com/libembeddedhal/liblpc40xx"
     description = "Drivers for the lpc40xx series of microcontrollers using libembeddedhal's abstractions."
     topics = ("peripherals", "hardware")
     settings = "os", "compiler", "arch", "build_type"
