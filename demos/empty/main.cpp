1f1c3fd16d7b019020697c4b72749d7fc6c5f268
Khalil Estell <kammcecorp@gmail.com>
2022-09-02 17:00:57 -0700

    Empty out empty.cpp

--- demos/empty/main.cpp
+++ demos/empty/main.cpp
@@ -1,18 +1,10 @@
 #define BOOST_LEAF_EMBEDDED
 #define BOOST_LEAF_NO_THREADS
 
-#include <libhal/percent.hpp>
-
-volatile int value = 0;
-int scale = 0;
-auto percent = hal::percent::from_ratio(1, 2);
+#include <libhal/error.hpp>
 
 int main()
 {
-  while (true) {
-    scale = percent * scale;
-    value = value + 1;
-  }
   return 0;
 }
 
