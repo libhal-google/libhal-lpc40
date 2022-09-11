aba8e391650477d9ce37a56806a2a340f9fa51cf
Khalil Estell <kammcecorp@gmail.com>
2022-09-11 09:45:34 -0700

    Add simple test package

--- include/liblpc40xx/internal/platform_check.hpp
+++ include/liblpc40xx/internal/platform_check.hpp
@@ -3,7 +3,7 @@
 #include <libhal/config.hpp>
 
 namespace hal::lpc40xx {
-consteval void compile_time_platform_check()
+constexpr void compile_time_platform_check()
 {
   static_assert(hal::is_platform("lpc40") || hal::is_a_test(),
                 "This driver can only be used for LPC40 series "
