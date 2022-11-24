549b47d001a1a1b1af60eb8753e0db0b25697020
Khalil Estell <kammcecorp@gmail.com>
2022-11-23 20:44:49 -0700

    Bump version to 0.2.6
    Fix serial API update

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ required_conan_version = ">=1.50.0"
 
 class LibLPC40xxConan(ConanFile):
     name = "liblpc40xx"
-    version = "0.2.5"
+    version = "0.2.6"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/liblpc40xx"
