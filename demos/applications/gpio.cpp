0867b04f6cfe1d04430b025a8116454c79c91401
Khalil Estell <kammce@google.com>
2023-08-28 21:12:24 -0700

    :sparkles: Add spi driver
    Resolves #56

--- demos/applications/gpio.cpp
+++ demos/applications/gpio.cpp
@@ -25,8 +25,8 @@ hal::status application()
   hal::cortex_m::dwt_counter clock(
     hal::lpc40::clock::get().get_frequency(hal::lpc40::peripheral::cpu));
 
-  auto button = HAL_CHECK((hal::lpc40::input_pin::get(0, 29)));
-  auto led = HAL_CHECK((hal::lpc40::output_pin::get(1, 18)));
+  auto button = HAL_CHECK(hal::lpc40::input_pin::get(0, 29));
+  auto led = HAL_CHECK(hal::lpc40::output_pin::get(1, 18));
 
   while (true) {
     // Checking level for the lpc40xx drivers NEVER generates an error so this
